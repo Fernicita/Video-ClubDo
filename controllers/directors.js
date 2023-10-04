8d6fd3928f08942a90b38e20dd9a37c53d312e49
Miriam <124909948+Fernicita@users.noreply.github.com>
2023-10-04 14:34:38 -0600

    Actualización de clase

--- controllers/directors.js
+++ controllers/directors.js
@@ -19,19 +19,45 @@ function list(req, res, next){ //corregir esto porque me sale un error
 } 
 
 function index(req, res, next){
-    res.send('Director index');
+    const id=req.params.id;
+    Director.findByPk(id)
+            .then(object => res.json(object))
+            .catch(err => res.send(err));
+
 }
 
 function replace(req, res, next){
-    res.send('Director replace');
+     const id = req.params.id;
+     Director.findByPk(id)
+             .then(object => {
+                const name = req.body.name ? req.body.name : "";
+                const lastName = req.body.lastName ? req.body.lastName : "";
+                object.update({
+                    name:name,
+                    lastName: lastName
+                }).then(obj => res.json(obj)) //es object tmb pero por el de arriba no puede tener el mismo nombre
+                  .catch(err => res.send(err));
+             }).catch(err => res.send(err));
 }
 
 function update(req, res, next){
-    res.send('Director update');
+    const id = req.params.id;
+        Director.findByPk(id).then(object => {
+            const name = req.body.name ? req.body.name : object.name;
+            const lastName = req.body.lastName ? req.body.lastName : object.lastName;
+            object.update({
+                name:name,
+                lastName:lastName
+            }).then(obj => res.json(obj))
+              .catch(err => res.send(err));
+        }).catch(err => res.send(err));
 }
 
 function destroy(req, res, next){
-    res.send('Director destroy');
+    const id = req.params.id;
+    Director.destroy({where:{id: id}})
+            .then(object => res.json(object))
+            .catch(err => res.send(err));
 }
 
 module.exports={create, list, index, update, replace, destroy}
--- db.js
+++ db.js
@@ -1,6 +1,11 @@
 const Sequelize = require('sequelize');
 
 const directorModel=require('./models/director'); //ruta relativa
+
+const genreModel= require('./models/genre');
+
+const movieModel = require('./models/movie');
+
 //establecer conexión con la base de datos estos elementos son los que se van a utilizar traer la imagen
 /*
     1)Nombre de la base de datos
@@ -14,9 +19,28 @@ const sequelize= new Sequelize('Video-clubDo', 'root', '123', {
 });
 
 const Director= directorModel(sequelize, Sequelize);
+
+const Genre= genreModel(sequelize, Sequelize);
+
+const Movie = movieModel(sequelize, Sequelize);
+
+//RELACIONES
+
+//un genero puede tener muchas peliculas
+Genre.hasMany(Movie, { as: 'movies'});
+//una pelicula tiene un género
+Movie.belongsTo(Genre, {as:'genre'});
+
+
+// Un director puede tener muchas películas 
+Director.hasMany(Movie, {as: 'movies'});
+
+//una pelicula tiene un director 
+Movie.belongsTo(Director, {as: 'director'});
+
 sequelize.sync({
     force:true
 }). then (()=>{
     console.log('Base de datos sincronizada');
 });
-module.exports = {Director};
+module.exports = {Director, Genre, Movie};
